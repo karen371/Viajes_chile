05cc55a28bacc85e425a0ab71a3a162d84766d4d
karen371 <karengodoy325@gmail.com>
2024-07-16 22:28:29 -0400

    arreglos en css y html, ademas de incorporar smooth scroll, tooltip e incorporacion de codigo js

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,25 @@ $(document).ready(function(){
       buttons.filter('[data-bs-slide-to="' + slideIndex + '"]').addClass('active');
     });
   });
+  /*Tooltip*/
+  document.addEventListener("DOMContentLoaded", function() {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl); 
+    });
+  });
+  /* Formulario*/
+  $(document).ready(function() {
+    $('#MyFormulario').submit(function(event) {
+      // Previene el envío por defecto del formulario
+      event.preventDefault();
+      let nombre = $('#Nombre').val();
+      if(nombre.trim() === ''){
+        alert('Por favor complete los campos');
+      }
+      else{
+        let mensaje = 'Mensajes recido, gracias por contactarnos ' + nombre;
+        alert(mensaje);
+      }
+    });
+  });
